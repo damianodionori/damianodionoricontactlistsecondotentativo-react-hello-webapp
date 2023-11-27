cda7ac39dc0402cfa021811f702648ffadd48701
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-27 18:49:28 +0000

    base structure

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,22 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			getAgenda: async () => {
 				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				const response = await fetch("https://playground.4geeks.com/apis/fake/contact/agenda/damianodionori");
+				const jsonResponse = await response.json();
 
-				//reset the global store
-				setStore({ demo: demo });
+				setStore({ contacts: jsonResponse});
+			},
+			
+			addContact: (contactData) => {
+
+			
+			
 			}
 		}
 	};
