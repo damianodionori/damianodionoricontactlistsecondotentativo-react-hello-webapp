a4ffc78a2fa35ad97e3a2c36db9128eb4cf09e26
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-12-01 12:08:02 +0000

    footbar

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "../../styles/footer.css";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with lots of <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="https://github.com/damianodionori" target="_blank">Damiano Dionori</a>
-		</p>
-	</footer>
+    <p>
+      Made with lots of <i className="fa fa-heart text-danger" /> by{" "}
+      <a href="https://github.com/damianodionori" target="_blank">Damiano Dionori</a>
+    </p>
+  </footer>
 );
