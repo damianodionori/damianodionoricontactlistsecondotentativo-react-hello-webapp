9143f0b0e49cb2f12df716dde6b74dcc8d6d3b80
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-12-01 12:47:51 +0000

    home

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -17,27 +17,34 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			addContact: async (contacts) => {
 				const newContact = {
-					"full_name": contacts.full_name,
-					"email": contacts.email,
-					"agenda_slug": "damianodionori",
-					"address": contacts.address,
-					"phone": contacts.phone
+				  full_name: contacts.full_name,
+				  email: contacts.email,
+				  agenda_slug: "damianodionori",
+				  address: contacts.address,
+				  phone: contacts.phone,
 				};
-
+			  
 				const store = getStore();
-				await fetch("https://playground.4geeks.com/apis/fake/contact", {
+				try {
+				  const response = await fetch("https://playground.4geeks.com/apis/fake/contact", {
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify(newContact),
-				});
-				if (!response.ok) {
+				  });
+			  
+				  if (!response.ok) {
 					console.error("Failed to add contact", response.statusText);
 					return;
+				  }
+			  
+				  const actions = getActions();
+				  actions.getAgenda();
+				  setStore({ contacts: [...store.contacts, newContact] });
+				} catch (error) {
+				  console.error("Error during fetch:", error);
 				}
-				const actions = getActions();
-				actions.getAgenda();
-				setStore({ contacts: [...store.contacts, newContact] });
-			}
+			  },
+			  
 		},
 
 		//deleteContact:
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,6 +4,6 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1 className="display-4"> Here is my Contact List</h1>
+		<h1 className="display-4"> Here is my Contact List Application</h1>
 	</div>
 );
